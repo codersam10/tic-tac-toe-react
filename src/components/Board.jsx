595cf508ecb5fe063430cea77d75060e75571da3
sam <savantsamarth@gmail.com>
2024-06-05 10:39:12 +0530

    feat- track and jump to recent  moves

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
-import Square from "./components/Square";
+import Square from "./Square";
 
-const Board = () => {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+const Board = ({xIsNext, squares, onPlay}) => {
 
   function handleClick(i) {
     if (squares[i] === null) {
       const nextSquares = squares.slice();
       xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
-      setSquares(nextSquares);
-      setXIsNext(!xIsNext);
+      onPlay(nextSquares);
     }
   }
 
@@ -21,6 +17,31 @@ const Board = () => {
   } else {
     status = `Next player: ${xIsNext ? "X" : "O"}`;
   }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <div className="board">
       <div className="status">{status}</div>
@@ -69,25 +90,5 @@ const Board = () => {
     </div>
   );
 };
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
 export default Board;
